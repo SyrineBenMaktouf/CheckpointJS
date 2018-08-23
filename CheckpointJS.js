1d362546b575f8fbdd298b12098619f44ae81528
SyrineBenMaktouf <42380457+SyrineBenMaktouf@users.noreply.github.com>
2018-08-23 12:57:34 +0200

    Add files via upload

---
+++ CheckpointJS.js
@@ -0,0 +1,22 @@
+
+function bold() {
+    document.getElementById("txt").style.fontWeight="bold"}
+    
+function italic() {
+    document.getElementById("txt").style.fontStyle="italic"}
+function changefontFamily() {
+    document.getElementById("txt").style.fontFamily=document.getElementById("btn4").value;
+}
+
+     
+ function changefontSize() {
+    document.getElementById("txt").style.fontSize=document.getElementById("btn3").value+"px"}
+function Underline() {
+    document.getElementById("txt").style.textDecoration="underline"}
+function Overline() {
+    document.getElementById("txt").style.textDecoration="overline"}   
+function changeColor() {
+    document.getElementById("txt").style.color="blue"}   
+function Justify() {
+    document.getElementById("txt").style.justifyContent ="space-between"}     
+      
